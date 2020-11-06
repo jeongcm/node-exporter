37840163356158374909245c48781d4469b805e1
jcm <jeongcm7429@datacommand.kr>
2020-11-06 10:34:08 +0900

    node-exporter node_meta label에 node_ip 추가

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,12 @@ if [ "${NODE_NAME}" == "" ]; then
     exit 1
 fi
 
-echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /node-meta.prom
+if [ "${NODE_IP}" == "" ]; then
+    echo "ERROR: NODE_IP ENV is required for this container to run"
+    exit 1
+fi
+
+echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", \
+node_name=\"$NODE_NAME\", node_ip=\"$NODE_IP\"} 1" > /node-meta.prom
 
 /bin/node_exporter --collector.textfile.directory=/
