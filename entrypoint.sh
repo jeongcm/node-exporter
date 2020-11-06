726854c2f33a93c79fe9c90f85df8831ad9f8a93
jcm <jeongcm7429@datacommand.kr>
2020-11-06 11:09:24 +0900

    NODE_IP를 env로 shell명령어를 통해서 받도록 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,10 @@ if [ "${NODE_NAME}" == "" ]; then
     exit 1
 fi
 
-NODE_IP=$(docker node inspect self --format '{{.Status.Addr}}')
+if [ "${NODE_IP}" == "" ]; then
+    echo "ERROR: NODE_IP ENV is required for this container to run"
+    exit 1
+fi
 
 echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", \
 node_name=\"$NODE_NAME\", node_ip=\"$NODE_IP\"} 1" > /node-meta.prom
