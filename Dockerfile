a8344c489b89c838b60b9397a2a2a6c7da8adaae
jcm <jeongcm7429@datacommand.kr>
2020-11-06 13:59:29 +0900

    NODE_IP를 entrypoint.sh에서 nslookup을 통해 받도록 수정

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,9 @@ FROM registry.datacommand.co.kr/node-exporter:v1.0.1
 
 ENV NODE_ID ""
 ENV NODE_NAME ""
-ENV NODE_IP ""
 
 USER root
 
-CMD ["export", "NODE_IP=$(docker node inspect self --format '{{.Status.Addr}}')"]
 COPY entrypoint.sh /
 
 ENTRYPOINT  [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,10 +10,7 @@ if [ "${NODE_NAME}" == "" ]; then
     exit 1
 fi
 
-if [ "${NODE_IP}" == "" ]; then
-    echo "ERROR: NODE_IP ENV is required for this container to run"
-    exit 1
-fi
+NODE_IP=$(nslookup -type=any $NODE_NAME | sed -n '5p'| awk '{print $2}')
 
 echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", \
 node_name=\"$NODE_NAME\", node_ip=\"$NODE_IP\"} 1" > /node-meta.prom
